f55013c79ed418f488e82e358bcbc4e26e17a775
Rogerio <dgelask8@gmail.com>
2020-06-02 17:59:28 -0300

    Módulo 13: Javascript Avançado - Propriedades Aula 02

---
+++ b7web/phpzp/modulo-02-javascript-avancado/aula02-propriedades/script.js
@@ -0,0 +1,12 @@
+function Animal() {
+    this.tipo = 'Cao';
+    this.cor = 'Preto';
+    this.nome = 'Mylow';
+    this.peso = '20';
+}
+
+var animal1 = new Animal();
+animal1.nome = 'Dolly';
+animal1.tipo = 'Gato';
+
+var animal2 = new Animal();
