456d2c28a895fd3c78d10e9587641a20b1f99fb4
Rogerio <dgelask8@gmail.com>
2020-07-02 17:42:58 -0300

    jQuery: Eventos de Browser - Aula 11

---
+++ b7web/phpzp/modulo-03-jquery/aula11-eventos-de-browser/script.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(window).bind('resize',function () {
+        console.log('mudou o tamanho');
+    });
+    /*$('#div').bind('scroll',function () {
+        console.log('scroll');
+        $(this).css('background', 'green');
+    });*/
+});
