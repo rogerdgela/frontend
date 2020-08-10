bccfedaa4ff18280e0d8bdca041a4ac3db235c26
Rogerio <dgelask8@gmail.com>
2020-08-10 13:59:36 -0300

    jQuery: Projeto: Projeto: Drag & Drop - Aula 35

---
+++ b7web/phpzp/modulo-03-jquery/aula35-projeto-drag-e-drop/script.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('#objeto').bind('mousedown',function (){
+        $('#objeto').bind('mousemove',function (event){
+            //console.log(event.originalEvent.pageX);
+            var x = event.originalEvent.pageX;
+            var y = event.originalEvent.pageY;
+
+            var width = $(this).width();
+            var height = $(this).height();
+
+            $(this).css('left', (x - (width / 2)) + 'px');
+            $(this).css('top', (y - (height / 2)) + 'px');
+        });
+    });
+
+    $('#objeto').bind('mouseup',function (){
+
+        $('#objeto').unbind('mousemove');
+    });
+});
