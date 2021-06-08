8645fe4482d0a2352f2ebf94c835002640f782f8
Rogerio Silva <dgelask8@gmail.com>
2021-06-08 19:38:49 -0300

    Módulo 29: NodeJS - Criando Módulos - Aula 05

---
+++ b7web/phpzp/modulo-24-nodejs/aula05-criando-modulos/matematica.js
@@ -0,0 +1,16 @@
+var Matematica = {
+    somar:function (x,y){
+          return x + y;
+    },
+    diminuir:function (x,y){
+        return x - y;
+    },
+    multiplicar:function (x,y){
+        return x * y;
+    },
+    dividir:function (x,y){
+        return x / y;
+    },
+};
+
+module.exports = Matematica;
---
+++ b7web/phpzp/modulo-24-nodejs/aula05-criando-modulos/teste.js
@@ -0,0 +1,6 @@
+let mat = require('./matematica');
+
+console.log('A soma de 1 + 2 = ' + mat.somar(1,2));
+console.log('A subtração de 1 - 2 = ' + mat.diminuir(1,2));
+console.log('A multiplicação de 1 * 2 = ' + mat.multiplicar(1,2));
+console.log('A divisão de 1 / 2 = ' + mat.dividir(1,2));
